5e55b3e16536f654c9b42a4aeed8401e4fc75c35
Abulkhair23 <78597371+Abulkhair23@users.noreply.github.com>
2021-02-18 11:42:34 +0600

    dd

--- week3notfinished/base.js
+++
@@ -1,8 +0,0 @@
-function CreateElement() {
-    var list = document.getElementById("mylist");
-    var task = document.getElementById("task-input").value;
-    var entry = document.createElement("li");
-    entry.appendChild(document.createTextNode(task));
-    list.appendChild(entry);
-}
-
